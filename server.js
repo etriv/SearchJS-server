fd51db0b5b7956e00d4b0cc254325094f94e391d
etriv <itai.riv@gmail.com>
2020-02-29 16:54:39 +0100

    Add ability to query for tweeter tweets and add env-vars support

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const Twit = require('twit');
+require('dotenv').config();
+
+var T = new Twit({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000,  // optional
+    strictSSL: true,        // optional
+})
 
 console.log('--- SERVER START ---');
 
@@ -11,6 +22,18 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Getting root... Yam yam yam!');
 });
+
+T.get('search/tweets', {
+    q: '#javascript AND -filter:replies AND -filter:retweets',
+    tweet_mode: 'extended',
+    lang: 'en',
+    result_type: 'recent',
+    count: 2
+},
+    function (err, data, response) {
+        console.log(data)
+    }
+)
 
 /////////////////////////////////
 // --- STARTING THE SERVER --- //
